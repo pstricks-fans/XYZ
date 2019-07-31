a68b15ec1d89967514f04546e4d0e22ff558cf20
pstricks-fans <50270635+pstricks-fans@users.noreply.github.com>
2019-07-31 13:34:29 +0700

    Squezing
    Removing unnecessary parts.

---
+++ XYZ/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace XYZ.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index() => View();
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            ViewBag.Message = "From Delete";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            TempData["Message"] = "From DeleteConfirmed";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- XYZ/Startup.cs
+++ XYZ/Startup.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using XYZ.Data;
 
 namespace XYZ
 {
@@ -11,8 +9,6 @@ namespace XYZ
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source = XYZ.db"));
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -22,7 +18,7 @@ namespace XYZ
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{area=Admin}/{controller=Person}/{action=Index}/{id?}");
+                    template: "{area=Admin}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
